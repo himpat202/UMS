bf4d38cdcd293f4796a2a4f7ea8437cc6d6844cd
Himanshu Pathak <himpat202@gmail.com>
2021-07-24 14:41:12 +0530

    change3

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -34,7 +34,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `/api/users/${id}`,
             "method" : "DELETE"
         }
 

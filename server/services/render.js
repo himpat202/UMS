c88ba7d927aa1240fa7e5ca8eec79a1360d744be
Himanshu Pathak <himpat202@gmail.com>
2021-07-24 12:42:27 +0530

    commit for change

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req,res) => {
     // make a get requests
-    axios.get('http://localhost:3000/api/users')
+    axios.get('/api/users')
    .then(function(response){
        // console.log(response.data)
         res.render('index',{users:response.data});
@@ -19,7 +19,7 @@ exports.add_user = (req,res) => {
 
 
 exports.update_user = (req, res) =>{
-    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+    axios.get('/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
